53c47feaac91c816c4d27d195b49d6624c5f06f7
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-07-18 11:01:57 +0200

    live ex

---
+++ resources/views/show.blade.php
@@ -0,0 +1,57 @@
+@extends("layouts.main-layout")
+
+@section("content")
+
+    <div class="text-center">
+        <h1 class="py-3">{{ $comic -> title}}</h1>
+        <img class="py-3" src="{{ $comic -> thumb }}" width="350px">
+        <div class="row justify-content-between py-3">
+            <span class="col bg-dark text-light mx-3 rounded">
+                {{ $comic -> series }}
+            </span>
+            <span class="col bg-dark text-light mx-3 rounded">
+                {{ $comic -> price }}
+            </span>
+        </div>
+        <div class="row justify-content-between py-3">
+            <span class="col bg-dark text-light mx-3 rounded">
+                {{ $comic -> sale_date }}
+            </span>
+            <span class="col bg-dark text-light mx-3 rounded">
+                {{ $comic -> type }}
+            </span>
+        </div>
+        <div class="row justify-content-between py-3">
+            <div class="col">
+                <h4>
+                    <b>
+                        Artists
+                    </b>
+                </h4>
+                <ul class="list-unstyled">
+                    @foreach (json_decode($comic -> artists) as $artistName)
+                        <li>
+                            {{ $artistName }}
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+            <div class="col">
+                <h4>
+                    <b>
+                        Writers
+                    </b>
+                </h4>
+                <ul class="list-unstyled">
+                    @foreach (json_decode($comic -> writers) as $writerName)
+                        <li>
+                            {{ $writerName }}
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+
+    </div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\MainController;
+
+Route :: get('/', [MainController :: class, 'index'])
+    -> name("index");
+Route :: get('/show/{id}', [MainController :: class, 'show'])
+    -> name('show');
+
+Route :: get('/create', [MainController :: class, 'create'])
+    -> name('create');
+Route :: post('/store', [MainController :: class, 'store'])
+    -> name('store');
