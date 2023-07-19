7b0b12c30bf282d1b7df0799f1c02416c21bf29b
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-07-19 13:10:27 +0200

    $*

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,9 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// $('#myModal').on('shown.bs.modal', function () {
+//     $('#myInput').trigger('focus')
+//   })
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,13 +12,38 @@
 
         <ul class="list-unstyled">
             @foreach ($comics as $comic)
-                <li>
+                <li class="my-3">
                     <a href="{{ route('show', $comic -> id) }}">
                         {{ $comic -> title }}
                     </a>
+                    <br>
+                    <a class="btn btn-primary mx-3" href="{{ route('edit', $comic -> id) }}">
+                        EDIT
+                    </a>
+                    <form
+                        class="d-inline"
+                        method="POST"
+                        action="{{ route('delete', $comic -> id) }}"
+                        onsubmit="return formSend(event)"
+                    >
+
+                        @csrf
+                        @method("DELETE")
+
+                        <input type="submit" class="btn btn-primary mx-3" value="DELETE">
+                    </form>
                 </li>
             @endforeach
         </ul>
     </div>
+
+    <script>
+        function formSend(e) {
+            if (window.confirm("Do you really want to delete this comic?"))
+                return;
+
+            e.preventDefault();
+        }
+    </script>
 
 @endsection
